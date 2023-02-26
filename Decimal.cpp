4b53fc23ab8fed7755ce92827136c3b5e4c0ea95
anshul_wd <91141762+heyansul@users.noreply.github.com>
2023-02-26 08:13:29 +0530

    Update Decimal.cpp

--- Decimal.cpp
+++ Decimal.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    //int i =0;
+    int i =0;
     int answer = 0;
     while(n!=0){
         int bit = n&1;
-        answer = (bit*pow(10))+answer;
+        answer = (bit*pow(10,i))+answer;
         n=n>>1;
-        //i = i+1;
+        i = i+1;
 
     }
     cout<< "Answer in Bit is "<<answer<<endl;
