bdf47a740defe7a22a0c21753491688993db3ab5
anshul_wd <91141762+heyansul@users.noreply.github.com>
2023-03-07 07:16:08 +0530

    Add files via upload

---
+++ pattern_4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i=1;
+    cin>>n;
+    while(i<=n){
+       int j= 1;
+       while(j<=n){
+       cout<<n-j+1;
+       j=j+1;
+    }
+    cout<<endl;
+    i=i+1;
+    }
+}
