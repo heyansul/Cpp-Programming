4524dd2fac9ad4a514cfbe8d3bbb0e6fb6c3cdaf
anshul_wd <91141762+heyansul@users.noreply.github.com>
2023-04-03 07:14:38 +0530

    Add files via upload

---
+++ Switch.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int a = 3;
+   switch (a)
+   {
+   case 1:
+    cout<< "My name is anshul"<<endl;
+    break;
+   case 2:
+   cout<< "My name is shubham"<<endl;
+   break;
+   default:
+   cout<<"This is the Deafalt"<<endl;
+    break;
+   }  
+}
